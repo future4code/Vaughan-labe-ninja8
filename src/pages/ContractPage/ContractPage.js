f1de64c6519af84af3491014390f1b65e05dd55a
Jessica Oliveira <jessicaoliveira@Jessicas-Mac-mini.local>
2022-02-03 10:59:37 -0300

    filtros de ordenação feitos

--- src/pages/ContractPage/ContractPage.js
+++ src/pages/ContractPage/ContractPage.js
@@ -1,99 +1,129 @@
-import React from 'react'
-import axios from 'axios'
-import {URL_BASE, Headers} from '../../constants/urls'
-import { ButtonsContainer, JobsCardContainer } from './styled'
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import { IconButton, CardContent, CardActions, Button, Typography } from '@mui/material'
-import styled from 'styled-components'
-
-
+import React from "react";
+import axios from "axios";
+import { URL_BASE, Headers } from "../../constants/urls";
+import { ButtonsContainer, JobsCardContainer } from "./styled";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import {
+  IconButton,
+  CardContent,
+  CardActions,
+  Button,
+  Typography,
+} from "@mui/material";
+import styled from "styled-components";
 
 export default class ContractPage extends React.Component {
   state = {
     buscaTitulo: "",
     jobs: [],
     precoMax: "",
-    precoMin: ""
-  }
-  
+    precoMin: "",
+    sortingParameter: "",
+  };
+
   componentDidMount() {
-    this.getJobs()
+    this.getJobs();
   }
   getJobs = () => {
-    axios.get(`${URL_BASE}/jobs`, Headers)
-    .then((res) => {
-      this.setState({jobs: res.data.jobs})
-      console.log(res.data.jobs)
-    })
-    .catch((err) => {
-      console.log(err.response.data)
-    })
-    
-  }
+    axios
+      .get(`${URL_BASE}/jobs`, Headers)
+      .then((res) => {
+        this.setState({ jobs: res.data.jobs });
+        console.log(res.data.jobs);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+  };
 
   onChangeBuscaTitulo = (event) => {
     this.setState({
-      buscaTitulo: event.target.value
-    })
-  }
+      buscaTitulo: event.target.value,
+    });
+  };
 
   onChangePrecoMax = (event) => {
     this.setState({
-      precoMax: event.target.value
-    })
-  }
+      precoMax: event.target.value,
+    });
+  };
   onChangePrecoMin = (event) => {
     this.setState({
-      precoMin: event.target.value
-    })
-  }
-  
-   
+      precoMin: event.target.value,
+    });
+  };
 
-  render () {
+  updateSortingParameter = (event) => {
+    this.setState({ sortingParameter: event.target.value });
+  };
 
-    const filteredList = this.state.jobs.filter(job => {
-      return job.title.toLowerCase().includes(this.state.buscaTitulo.toLowerCase()) || job.description.toLowerCase().includes(this.state.buscaTitulo)
-    })
-    
-    .filter(job => {
-      return this.state.precoMax === "" || job.price <= this.state.precoMax
-    })
-    .filter(job => {
-      return this.state.precoMin === "" || job.price >= this.state.precoMin
-    })
+  render() {
+    const filteredList = this.state.jobs
+      .filter((job) => {
+        return (
+          job.title
+            .toLowerCase()
+            .includes(this.state.buscaTitulo.toLowerCase()) ||
+          job.description.toLowerCase().includes(this.state.buscaTitulo)
+        );
+      })
+      .filter((job) => {
+        return this.state.precoMax === "" || job.price <= this.state.precoMax;
+      })
+      .filter((job) => {
+        return this.state.precoMin === "" || job.price >= this.state.precoMin;
+      })
+      .sort((a, b) => {
+        switch (this.state.sortingParameter) {
+          case "title":
+            return a.title.localeCompare(b.title);
+          case "dueDate":
+            return (
+              new Date(a.dueDate).getTime() - new Date(b.dueDate).getTime()
+            );
+          case "price-ascending":
+            return a.price - b.price;
+          case "price-descending":
+            return b.price - a.price;
+          default:
+            return a.title.localeCompare(b.title);
+        }
+      });
 
-    const jobsList = filteredList.map((list) => 
-    // list.list2.map((listing) => 
-    // )
-    <JobsCardContainer key={list.id}>
-      <CardContent>
-      <Typography variant="h5" component="div">
-      {list.title}
-        </Typography>
-        <Typography variant="body2">
-        <strong>Disponivél até:</strong> {list.dueDate}<br />
-         por <strong>R${list.price},00</strong>
-        </Typography>
-        {list.description}
-      </CardContent>
-      <CardActions>
-        <ButtonsContainer>
-          <Button onClick={this.props.handleContractDetailPage} size="small">
-            Ver Detalhes</Button>
-          <IconButton  size="small"><AddShoppingCartIcon/></IconButton>
-        </ButtonsContainer>
-      </CardActions>
-    </JobsCardContainer>
-    )
-    
-    
+    const jobsList = filteredList.map((list) => (
+      // list.list2.map((listing) =>
+      // )
+      <JobsCardContainer key={list.id}>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            {list.title}
+          </Typography>
+          <Typography variant="body2">
+            <strong>Disponivél até:</strong> {list.dueDate}
+            <br />
+            por <strong>R${list.price},00</strong>
+          </Typography>
+          {list.description}
+        </CardContent>
+        <CardActions>
+          <ButtonsContainer>
+            <Button onClick={this.props.handleContractDetailPage} size="small">
+              Ver Detalhes
+            </Button>
+            <IconButton size="small">
+              <AddShoppingCartIcon />
+            </IconButton>
+          </ButtonsContainer>
+        </CardActions>
+      </JobsCardContainer>
+    ));
+
     return (
       <div>
-          <button onClick={this.props.handleHomePage}>Home</button>
-          <button onClick={this.props.handleCartPage}>Carrinho</button>
-          <h2>Contratos</h2>
-          <div>
+        <button onClick={this.props.handleHomePage}>Home</button>
+        <button onClick={this.props.handleCartPage}>Carrinho</button>
+        <h2>Contratos</h2>
+        <div>
           <input
             placeholder="Busca por título ou descrição"
             value={this.state.buscaTitulo}
@@ -111,10 +141,19 @@ export default class ContractPage extends React.Component {
             value={this.state.precoMin}
             onChange={this.onChangePrecoMin}
           />
+          <select
+            name="sort"
+            value={this.state.sortingParameter}
+            onChange={this.updateSortingParameter}
+          >
+            <option value="title">Título</option>
+            <option value="dueDate">Prazo</option>
+            <option value="price-ascending">Preço - Crescente</option>
+            <option value="price-descending">Preço - Decrescente</option>
+          </select>
         </div>
-          {jobsList}
+        {jobsList}
       </div>
-      
-    )
+    );
   }
 }

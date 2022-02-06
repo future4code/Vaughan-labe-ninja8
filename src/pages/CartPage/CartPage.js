d610bbf9a5751d448762d9f3edec71a07b42aff4
Jessica Oliveira <jessicaoliveira@Jessicas-Mac-mini.local>
2022-02-06 12:18:58 -0300

    inicio carrinho apenas com funções de add e remover produto

--- src/pages/CartPage/CartPage.js
+++ src/pages/CartPage/CartPage.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+// const CartCard = styled.div`
+//   display: flex;
+// `;
 
 export default class CartPage extends React.Component {
-  render () {
+  render() {
+    // const productOnCart = this.props.cart.map((item) => {
+    //   return <CartCard title={item.title} price={item.price} id={item.id} />;
+    // });
     return (
-      <div>
-        <button onClick={this.props.handleHomePage}>Home</button>
-        <button onClick={this.props.handleContractPage}>Voltar aos contratos</button>
-        <h2>Carrinho</h2>
-        <button>Contratar servições(Aqui entra uma função)</button>
-      </div>
-    )
+      <>
+        <div>
+          <p>{this.props.title}</p>
+          <p>{this.props.price}</p>
+          {/* <button onClick={() => this.props.deleteFromCart(this.props.id)}>
+            Remover
+          </button> */}
+        </div>
+        <div>
+          <button onClick={this.props.handleHomePage}>Home</button>
+          <button onClick={this.props.handleContractPage}>
+            Voltar aos contratos
+          </button>
+          <h2>Carrinho</h2>
+          <button>Contratar serviços(Aqui entra uma função)</button>
+        </div>
+      </>
+    );
   }
 }
